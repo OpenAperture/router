d2124e40d229cdd5b114a2784140a8d0e89a584d
Jordan Day <jordan@roboninja.com>
2015-05-08 21:20:36 -0500

    add some more tests for the reverse proxy module

--- test/reverse_proxy_test.exs
+++ test/reverse_proxy_test.exs
@@ -7,13 +7,15 @@ defmodule OpenAperture.Router.ReverseProxy.Test do
   alias OpenAperture.Router.ReverseProxy
   alias OpenAperture.Router.ReverseProxy.Backend
   alias OpenAperture.Router.ReverseProxy.BufferedResponseBodyHandler
+  alias OpenAperture.Router.ReverseProxy.ChunkedResponseBodyHandler
   alias OpenAperture.Router.ReverseProxy.Client
   alias OpenAperture.Router.RouteCache
 
   setup do
     :meck.new :cowboy_req
     :meck.new Backend
-    :meck.new BufferedResponseBodyHandler, [:passthrough]
+    :meck.new BufferedResponseBodyHandler
+    :meck.new ChunkedResponseBodyHandler
     :meck.new Client, [:passthrough]
     :meck.new HttpRequestUtil, [:passthrough]
     :meck.new ReverseProxy, [:passthrough]
@@ -68,5 +70,89 @@ defmodule OpenAperture.Router.ReverseProxy.Test do
     result = proxy_request(:req, "/", :https)
 
     assert result == {:ok, :req9, 200}
+  end
+
+  test "should use the ChunkedResponseBodyHandler for chunked responses" do
+    method = :get
+    host = "test"
+    port = 80
+    is_https = false
+    headers = [{"content-type", "text/plain"}]
+    response_headers = [{"transfer-encoding", "chunked"}]
+    url = "http://test"
+    :meck.expect HttpRequestUtil, :get_backend_url, [{[:req1, host, port, is_https], {url, :req2}}]
+    :meck.expect Backend, :start_request, [{[method, url, headers, false], {:ok, :pid, 1}}]
+    :meck.expect :cowboy_req, :set_meta, [{[:response_type, :chunked, :req2], :req3}]
+    :meck.expect Client, :start_chunked_reply, [{[:req3, "200 OK", response_headers], {:req4, 1}}]
+    :meck.expect ChunkedResponseBodyHandler, :handle, [{[:req4, :pid], {:ok, :req5, 100}}]
+
+    send(self, {:backend_request_initial_response, :pid, 200, "OK", response_headers, 100})
+
+    result = proxy_request(method, :req1, host, port, is_https, headers)
+
+    assert result == {:ok, :req5, 100}
+  end
+
+  test "should use BufferedResponseBodyHandler for a buffered response" do
+    method = :get
+    host = "test"
+    port = 80
+    is_https = false
+    headers = [{"content-type", "text/plain"}]
+    response_headers = [{"content-length", "11"}]
+    url = "http://test"
+
+    :meck.expect HttpRequestUtil, :get_backend_url, [{[:req1, host, port, is_https], {url, :req2}}]
+    :meck.expect Backend, :start_request, [{[method, url, headers, false], {:ok, :pid, 1}}]
+    :meck.expect :cowboy_req, :set_meta, [{[:response_type, :buffered, :req2], :req3}]
+    :meck.expect BufferedResponseBodyHandler, :handle, [{[:req3, :pid, "200 OK", response_headers], {:ok, :req4, 100}}]
+
+    send(self, {:backend_request_initial_response, :pid, 200, "OK", response_headers, 100})
+
+    result = proxy_request(method, :req1, host, port, is_https, headers)
+
+    assert result == {:ok, :req4, 100}
+  end
+
+  test "should send a reply and set response_type to streaming for a large response" do
+    method = :get
+    host = "test"
+    port = 80
+    is_https = false
+    headers = [{"content-type", "text/plain"}]
+    response_headers = [{"content-length", "100000000000"}]
+    url = "http://test"
+
+    :meck.expect HttpRequestUtil, :get_backend_url, [{[:req1, host, port, is_https], {url, :req2}}]
+    :meck.expect Backend, :start_request, [{[method, url, headers, false], {:ok, :pid, 1}}]
+    :meck.expect :cowboy_req, :set_meta, [{[:response_type, :streaming, :req2], :req3}]
+    :meck.expect Client, :send_reply, [{[:req3, "200 OK", response_headers], {:req4, 100}}]
+
+    send(self, {:backend_request_initial_response, :pid, 200, "OK", response_headers, 100})
+
+    result = proxy_request(method, :req1, host, port, is_https, headers)
+
+    assert result == {:ok, :req4, 200}
+  end
+
+  test "should send a reply and set response_type to streaming for a response with no transfer-encoding=chunked header and no content-length header" do
+    method = :get
+    host = "test"
+    port = 80
+    is_https = false
+    headers = [{"content-type", "text/plain"}]
+    response_headers = [{"content-type", "text/plain"}]
+    url = "http://test"
+
+    :meck.expect HttpRequestUtil, :get_backend_url, [{[:req1, host, port, is_https], {url, :req2}}]
+    :meck.expect Backend, :start_request, [{[method, url, headers, false], {:ok, :pid, 1}}]
+    :meck.expect :cowboy_req, :set_meta, [{[:response_type, :streaming, :req2], :req3}]
+    :meck.expect Client, :send_reply, [{[:req3, "200 OK", response_headers], {:req4, 100}}]
+
+    send(self, {:backend_request_initial_response, :pid, 200, "OK", response_headers, 100})
+
+    result = proxy_request(method, :req1, host, port, is_https, headers)
+
+    assert result == {:ok, :req4, 200}
   end
 end

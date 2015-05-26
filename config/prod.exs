663fd30b74fdc8c51e32809aa78d1dd57dbc212c
jordan0day <jordan.day@perceptivesoftware.com>
2015-05-26 10:48:14 -0500

    retrieve prod config from env

--- config/prod.exs
+++ config/prod.exs
@@ -2,7 +2,11 @@ use Mix.Config
 
 config :openaperture_router,
   env: :prod,
-  http_port: System.get_env("OPENAPERTURE_ROUTER_HTTP_PORT")
+  http_port: System.get_env("OPENAPERTURE_ROUTER_HTTP_PORT"),
+  oauth_url: System.get_env("OPENAPERTURE_OAUTH_URL"),
+  client_id: System.get_env("OPENAPERTURE_ROUTER_CLIENT_ID"),
+  client_secret: System.get_env("OPENAPERTURE_ROUTER_CLIENT_SECRET"),
+  route_server_url: System.get_env("OPENAPERTURE_ROUTER_MANAGER_URL")
 
 config :logger, :console,
   level: :info
